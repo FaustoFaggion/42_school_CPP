90ad6fd01a57b6b4e01b06005b9b50cf40ac116f
FaustoFaggion <faustofaggion@hotmail.com>
2022-10-24 15:08:16 -0300

    CPP00 ex00 was unfinished. Finished now

--- CPP00/ex00/main.cpp
+++ CPP00/ex00/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 int	main(int argc, char *argv[]){
-	if (argc == 1)
+	if (argc != 2)
 		std::cout << "LOUD AND UNBEARABLE FEEDBACK NOISE" << std::endl;
 	else
 	{
-		
+		const std::string str = argv[1];
+		for (size_t i = 0; i < str.length(); i++)
+			std::cout << static_cast<char>(std::toupper(str.at(i)));
+		std::cout << std::endl;
 	}
 	 
 }
