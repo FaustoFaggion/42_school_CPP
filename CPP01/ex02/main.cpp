cb0f8bd6d1fe68e3649bab92aff90a929d9eda6b
FaustoFaggion <faustofaggion@hotmail.com>
2022-10-24 15:36:02 -0300

    CPP01 ex02 Done again

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int	main(void) {
+
+	std::string	str = "Hi THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "memory addr str: " << &str << std::endl;
+	std::cout << "memory addr stringPTR: " << &stringPTR << std::endl;
+	std::cout << "memory addr stringREF: " << &stringREF << std::endl;
+	
+	std::cout << "str: " << str << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+	return (0);
+}
