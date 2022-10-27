a7e638dd6aeaa3d338a4a50a48e7ee2aed40d4df
FaustoFaggion <faustofaggion@hotmail.com>
2022-10-27 17:16:43 -0300

    CPP08 ex00 Done

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -1,6 +1,31 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <list>
+#include <queue>
+#include <exception>
 
+template<typename T>
+void	easyfind(T &value1, int value2) {
+	
+	for (typename T::iterator it = value1.begin(); it != value1.end(); it++) {
+			if (*it == value2) {
+				std::cout << *it << std::endl;
+				return ;
+			}
+	}
+	std::cout << "Error: number not found!!!" << value2 << std::endl;
+}
+
+/*
+class	NotFoundException : public std::exception {
+	const char		*what() const throw() {
+		return ("grade too low");
+	}
+};
+*/
 
 #endif
---
+++ CPP08/ex00/main.cpp
@@ -0,0 +1,28 @@
+#include "easyfind.hpp"
+
+int main(void) {
+
+	std::vector<int> vec;
+	std::list<int> lis;
+	std::queue<int> que;
+	vec.reserve(6);
+
+	for (long unsigned int i = 0; i < 10; i++) {
+		vec.push_back(i);
+	}
+
+	for (long unsigned int i = 0; i < 10; i++) {
+		lis.push_front(i + 10);
+	}
+
+	for (long unsigned int i = 0; i < 10; i++) {
+		lis.push_front(i + 100);
+	}
+
+	std::cout << "VEC\n" << std::endl;
+	easyfind(vec, 13);
+	std::cout << "LIST\n" << std::endl;
+	easyfind(lis, 12);
+	std::cout << "QUEUE\n" << std::endl;
+	easyfind(lis, 102);
+}
