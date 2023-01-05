67973b6b35dfe551a57894472cd9c9dde96aaa7a
FaustoFaggion <faustofaggion@hotmail.com>
2023-01-05 11:54:41 -0300

    CPP00 ex00. It was taken the first argument only

--- CPP00/ex00/main.cpp
+++ CPP00/ex00/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
 int	main(int argc, char *argv[]){
-	if (argc != 2)
+	if (argc == 1)
 		std::cout << "LOUD AND UNBEARABLE FEEDBACK NOISE" << std::endl;
 	else
 	{
-		const std::string str = argv[1];
-		for (size_t i = 0; i < str.length(); i++)
-			std::cout << static_cast<char>(std::toupper(str.at(i)));
+		for (int i = 1; i < argc; i++) {
+			const std::string str = argv[i];
+			for (size_t i = 0; i < str.length(); i++)
+				std::cout << static_cast<char>(std::toupper(str.at(i)));
+		}
 		std::cout << std::endl;
 	}
 	 
