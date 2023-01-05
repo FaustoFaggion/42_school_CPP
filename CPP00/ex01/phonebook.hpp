dd3b42df24abc469d394c732f6d725dbca571750
FaustoFaggion <faustofaggion@hotmail.com>
2023-01-05 15:44:06 -0300

    CPP00 ex01. only methods addContact and searchContact are public

--- CPP00/ex01/phonebook.hpp
+++ CPP00/ex01/phonebook.hpp
@@ -12,21 +12,19 @@ class PhoneBook {
 		PhoneBook(void);
 		~PhoneBook(void);
 
+	private:
+		int	nameValidation(std::string name);
+		int	phoneValidation(std::string phone);
+		int	secretValidation(std::string secret);
+		std::string	isSizeOk(std::string paramter);
 		void setContact(int i, std::string f_name, std::string l_name,
 			std::string n_name, std::string phone, std::string secret);
-
-		int	nameValidation(std::string name);
-
-		int	phoneValidation(std::string phone);
-		
-		int	secretValidation(std::string secret);
-
+	
+	public:
 		void	addContact(std::string f_name, std::string l_name,
-			std::string n_name, std::string phone, std::string secret);
-		
+			std::string n_name, std::string phone, std::string secret);	
 		void	searchContact(void);
 
-		std::string	isSizeOk(std::string paramter);
 };
 
 #endif
