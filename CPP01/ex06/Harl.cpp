5ee7660309ab6b88d6c6369560e09a9e4dc57c08
FaustoFaggion <faustofaggion@hotmail.com>
2023-01-21 14:43:01 -0300

    CPP01 ex06 use 'for' in swich cases

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -7,25 +7,33 @@ Harl::~Harl(void){
 }
 
 void	Harl::debug(void){
+    std::cout << "[ " << "DEBUG" << " ]" << std::endl;
 	std::cout << "I love having extra bacon for my ";
 	std::cout << "7XL-double-cheese-triple-pickle-special-ketchup burger. ";
 	std::cout << "I really do!" << std::endl;
+    std::cout<< std::endl;
 }
 
 void	Harl::info(void){
+    std::cout << "[ " << "INFO" << " ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. ";
 	std::cout << "You didn’t put enough bacon in my burger! If you did, ";
 	std::cout << "I wouldn’t be asking for more!" << std::endl;
+    std::cout<< std::endl;
 }
 
 void	Harl::warning(void){
+    std::cout << "[ " << "WARNING" << " ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. ";
 	std::cout << "I’ve been coming for years whereas you started "; 
 	std::cout << "working here since last month." << std::endl;
+    std::cout<< std::endl;
 }
 
 void	Harl::error(void){
+    std::cout << "[ " << "ERROR" << " ]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout<< std::endl;
 }
 
 void Harl::notValid(void){
@@ -41,27 +49,25 @@ void	Harl::complain(std::string level){
 	
     opt = 4;
     for (int i = 0; i < 4; i++){
-		if (!levelLog[i].compare(level))
+		if (!levelLog[i].compare(level)) {
             opt = i;
+            break ;
+        }
 	}
     switch(opt){
         case 0:
-            std::cout << "[ " << "DEBUG" << " ]" << std::endl;
-            (this->*function[opt])();
-            std::cout<< std::endl;
-			[[fallthrough]];
+            for (int i = opt; i < 4; i++)
+                (this->*function[i])();
+            break;
         case 1:
-            std::cout << "[ " << "INFO" << " ]" << std::endl;
-            (this->*function[opt])();
-            std::cout<< std::endl;
-			[[fallthrough]];
+            for (int i = opt; i < 4; i++)
+                (this->*function[i])();
+            break;
         case 2:
-            std::cout << "[ " << "WARNING" << " ]" << std::endl;
-            (this->*function[opt])();
-            std::cout<< std::endl;
-			[[fallthrough]];
+            for (int i = opt; i < 4; i++)
+                (this->*function[i])();
+            break;
         case 3:
-            std::cout << "[ " << "ERROR" << " ]" << std::endl;
             (this->*function[opt])();
             break;
         case 4:
