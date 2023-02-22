022bda3c942dcca0ec9c882dc9e37d461023900f
FaustoFaggion <faustofaggion@hotmail.com>
2023-02-22 12:40:13 -0300

    CPP06 ex01 done

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -1,16 +1,61 @@
 #include "Data.hpp"
 
+struct myStruct
+{
+	int		x;
+	int		y;
+	char	c;
+	bool	b;
+};
+
 int	main(void) {
 
-	Data		a("Joao e Raoni", 33);
-	uintptr_t	b;
-	Data		*c;
+	{
 
-	b = serialize(&a);
+		Data		a("Joao e Raoni", 33);
+		uintptr_t	b;
+		Data		*c;
 
-	c = deserialize(b);
-	std::cout<< "a: " << a << std::endl;
-	std::cout<< "b: " << b << std::endl;
-	std::cout<< "c: " << *c << std::endl;
+		b = serialize(&a);
+
+		c = deserialize(b);
+		std::cout<< "a: " << a << std::endl;
+		std::cout<< "b: " << b << std::endl;
+		std::cout<< "c: " << *c << std::endl;
+	}
+	{
+		myStruct	s;
+		s.x = 5;
+		s.y = 10;
+		s.c = 'a';
+		s.b = true;
+
+		int *p = reinterpret_cast<int *>(&s);
+		std::cout << *p << std::endl;
+		p++;
+		std::cout << *p << std::endl;
+		p++;
+		char *c = reinterpret_cast<char *>(p);
+		std::cout << *c << std::endl;
+		c++;
+		bool *b = reinterpret_cast<bool *>(c);
+		std::cout << *b << std::endl;
+	
+		std::cout << "static cast" << std::endl;
+		s.x	 = s.c;
+		std::cout << s.x << std::endl;
+		s.x	 = static_cast<int>(s.b);
+		std::cout << s.x << std::endl;
+	}
+	{
+		char	c;
+		int		*p = (int*)&c;
+		*p = 5;
+
+		std::cout << *p << std::endl;
+		//int		*ip = static_cast<int*>(&c);
+	}
+
+
 
 }
